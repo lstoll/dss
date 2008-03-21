16af1031e62050c1637c9c8c363d1af521b85c51
Lincoln Stoll <lstoll@lstoll.net>
2008-03-21 23:32:45 +1100

    Adding step output, moving packaging dir to its own var (DRY'ing up) adding build command... oops.
    committer: Lincoln Stoll <lstoll@lstoll.net>
    
    --HG--
    extra : convert_revision : 786ce4ec2a4d81ff9ee87829f1f5bb79637c1607

--- builddpkg.sh
+++ builddpkg.sh
@@ -4,31 +4,38 @@
 
 PLAT=`uname`
 VER=`cat revision.h | grep "kVersionString \"" | cut -d \" -f 2`
+PACKAGING_DIR=$PACKAGING_DIR
 
 
 # Build the tarball first - this will handle getting everything compiled
+echo '--- Building linux tarball - to trigger build process'
 ./buildtarball $VER
 # Remove the tarball & clean the dist staging dir
-rm DarwinStreamingSrvr$VER-$PLAT.tar.gz
-rm -rf DarwinStreamingSrvr$VER-$PLAT
+echo '--- Removing results of tarball build'
+rm $PACKAGING_DIR.tar.gz
+rm -rf $PACKAGING_DIR
 
 # Re-create the root in the dist dir, but not flattened
-./DSS_MakeRoot DarwinStreamingSrvr$VER-$PLAT $VER
+echo '--- Creating unflatted dist directory'
+./DSS_MakeRoot $PACKAGING_DIR $VER
 
 # Create and copy the debian files
-mkdir DarwinStreamingSrvr$VER-$PLAT/DEBIAN
-cp -r dpkg/* DarwinStreamingSrvr$VER-$PLAT/DEBIAN/
+echo '--- Creating debian control directory, and copying files over'
+mkdir $PACKAGING_DIR/DEBIAN
+cp -r dpkg/* $PACKAGING_DIR/DEBIAN/
 
+echo '--- Appending version to control file'
 # Append the version number to the control file
-echo 'Version: '$VERSION >> DarwinStreamingSrvr$VER-$PLAT/DEBIAN/control
-# Set the arch to i386 - its the only one supported.
-echo 'Architecture: i386' >> DarwinStreamingSrvr$VER-$PLAT/DEBIAN/control
+echo 'Version: '$VERSION >> $PACKAGING_DIR/DEBIAN/control
 
+echo '--- creating the init.d dir, moving script from it to control dir'
 # Create the init.d dir in the dist dir, move script over.
-mkdir DarwinStreamingSrvr$VER-$PLAT/etc/init.d
-mv DarwinStreamingSrvr$VER-$PLAT/DEBIAN/streamingserver DarwinStreamingSrvr$VER-$PLAT/etc/init.d/
+mkdir $PACKAGING_DIR/etc/init.d
+mv $PACKAGING_DIR/DEBIAN/streamingserver $PACKAGING_DIR/etc/init.d/
 
+echo '--- Re-naming the config files to .dist'
 # Re-name all the config files to .dist
-for f in DarwinStreamingSrvr$VER-$PLAT/etc/streaming/*; do mv $f $f.dist; done
+for f in $PACKAGING_DIR/etc/streaming/*; do mv $f $f.dist; done
 
 # Build the package
+dpkg -b $PACKAGING_DIR
