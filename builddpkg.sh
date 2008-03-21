4cf4850a13c3951fc348909f38faf7eee678b8db
Lincoln Stoll <lstoll@lstoll.net>
2008-03-21 23:08:36 +1100

    Updated to set the version by reading revision.h, rather than taking it as an (optional) command line parameter
    committer: Lincoln Stoll <lstoll@lstoll.net>
    
    --HG--
    extra : convert_revision : e4bc9c04c3e7c2af1d5b689fd4c50823155333ff

--- builddpkg.sh
+++ builddpkg.sh
@@ -3,32 +3,32 @@
 ##Build script for Darwin Streaming Server debian package
 
 PLAT=`uname`
-VERSION=`cat revision.h | grep "kVersionString \"" | cut -d \" -f 2`
+VER=`cat revision.h | grep "kVersionString \"" | cut -d \" -f 2`
 
 
 # Build the tarball first - this will handle getting everything compiled
 ./buildtarball
 # Remove the tarball & clean the dist staging dir
-rm DarwinStreamingSrvr$1-$PLAT.tar.gz
-rm -rf DarwinStreamingSrvr$1-$PLAT
+rm DarwinStreamingSrvr$VER-$PLAT.tar.gz
+rm -rf DarwinStreamingSrvr$VER-$PLAT
 
 # Re-create the root in the dist dir, but not flattened
-./DSS_MakeRoot DarwinStreamingSrvr$1-$PLAT $1
+./DSS_MakeRoot DarwinStreamingSrvr$VER-$PLAT $VER
 
 # Create and copy the debian files
-mkdir DarwinStreamingSrvr$1-$PLAT/DEBIAN
-cp -r dpkg/* DarwinStreamingSrvr$1-$PLAT/DEBIAN/
+mkdir DarwinStreamingSrvr$VER-$PLAT/DEBIAN
+cp -r dpkg/* DarwinStreamingSrvr$VER-$PLAT/DEBIAN/
 
 # Append the version number to the control file
-echo 'Version: '$VERSION >> DarwinStreamingSrvr$1-$PLAT/DEBIAN/control
+echo 'Version: '$VERSION >> DarwinStreamingSrvr$VER-$PLAT/DEBIAN/control
 # Set the arch to i386 - its the only one supported.
-echo 'Architecture: i386' >> DarwinStreamingSrvr$1-$PLAT/DEBIAN/control
+echo 'Architecture: i386' >> DarwinStreamingSrvr$VER-$PLAT/DEBIAN/control
 
 # Create the init.d dir in the dist dir, move script over.
-mkdir DarwinStreamingSrvr$1-$PLAT/etc/init.d
-mv DarwinStreamingSrvr$1-$PLAT/DEBIAN/control/streamingserver DarwinStreamingSrvr$1-$PLAT/etc/init.d/
+mkdir DarwinStreamingSrvr$VER-$PLAT/etc/init.d
+mv DarwinStreamingSrvr$VER-$PLAT/DEBIAN/control/streamingserver DarwinStreamingSrvr$VER-$PLAT/etc/init.d/
 
 # Re-name all the config files to .dist
-for f in DarwinStreamingSrvr$1-$PLAT/etc/streaming/*; do mv $f $f.dist; done
+for f in DarwinStreamingSrvr$VER-$PLAT/etc/streaming/*; do mv $f $f.dist; done
 
 # Build the package
